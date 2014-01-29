03a838bb4181231745dccc0767444742a9619032
DavidNester <14nesterd@gmail.com>
2014-01-29 12:46:28 -0500

    Update ChessPieces.py

--- ChessPieces.py
+++ ChessPieces.py
@@ -2,3 +2,20 @@ class ChessPiece(object):
   def __init__(self,type,color): 
     this.type = type
     this.color = color
+    this.lastMove = 0
+    
+  def IsValidMove(self,Board, fromRow, fromCol, toRow, toCol, turn, turnNumber)
+    if !(InRange(fromRow) and InRange(fromCol) and InRange(toRow) and InRange(toCol)):
+      return False
+    elif fromRow == toRow and fromCol == toCol:
+      return False
+    else:
+      return True
+  
+  def InRange(self,value)
+    if 0<=value and value<8:
+      return True
+    else:
+      return False
+  
+    
