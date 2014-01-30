9f084823e5793fe4f6e4a91942c1ec1851c675c4
DavidNester <14nesterd@gmail.com>
2014-01-30 10:36:25 -0500

    Update Knight.py

--- Knight.py
+++ Knight.py
@@ -4,4 +4,26 @@ class Knight(ChessPiece):
     
     @overrides(IsValidMove)
     def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
+        if not (super(King,self).IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber)):
+            return False
+        if abs(toRow-fromRow) > 2 or abs(toCol-fromRow) > 2 or toRow == fromRow or toCol == fromCol:
+            return False
+        elif abs(toRow-fromRow) == 2 and abs(toCol-fromCol) == 1:
+            #Unsure about translation
+            if board[toRow,toCol].ChessPiece == None:
+                return True
+            elif board[toRow,toCol].ChessPiece.PieceColor != turn:
+                return True
+            else:
+                return False
+        elif abs(toRow-fromRow) == 1 and abs(toCol-fromCol) == 2:
+            if board[toRow,toCol].ChessPiece == None:
+                return True
+            elif board[toRow,toCol].ChessPiece.PieceColor != turn:
+                return True
+            else:
+                return False
+        else:
+            return False
+        
         
