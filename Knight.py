cbedc2952a971343fdc167e7b2a84d2f1f770643
DavidNester <14nesterd@gmail.com>
2014-01-30 10:08:14 -0500

    Update Knight.py

--- Knight.py
+++ Knight.py
@@ -1,5 +1,6 @@
 class Knight(ChessPiece):
-    pass
+    def __init__(self,color):
+        super(Knight,self).__init__(self,Knight,color)
     
     @overrides(IsValidMove)
     def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
