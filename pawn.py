4eb496c0ea2b9f59f58f39631a1025f43fc864cb
DavidNester <14nesterd@gmail.com>
2014-01-30 10:08:22 -0500

    Update pawn.py

--- pawn.py
+++ pawn.py
@@ -1,5 +1,6 @@
-class Knight(ChessPiece):
-    pass
+class Pawn(ChessPiece):
+    def __init__(self,color):
+        super(Pawn,self).__init__(self,Pawn,color)
     
     @overrides(IsValidMove)
     def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
