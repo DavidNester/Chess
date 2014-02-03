638a10001b24b17898a496e15db5cdaf9961ae6b
DavidNester <14nesterd@gmail.com>
2014-02-03 13:31:46 -0500

    Update bishop.py

--- bishop.py
+++ bishop.py
@@ -1,20 +1,24 @@
 class Bishop(ChessPiece):
+	
+	
     def __init__(self,color):
         super(Bishop,self).__init__(self,Bishop,color)
     
     @overrides(IsValidMove)
-    def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
-        if not (super(Bishop,self).IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber)):
+    def override IsValidMove(board,fromRow,
+                             fromCol,toRow,toCol,turn,turnNumber):
+        if not (super(Bishop,self).IsValidMove(board,fromRow,fromCol,
+                                               toRow,toCol,turn,turnNumber)):
             return False
         if toCol == fromCol or toRow == fromRow:
                 return False
         if abs(fromRow - toRow) == abs(fromCol - toCol):
             # C#: for (int i = 1; i < Math.Abs(fromRow-toRow); i++)
             for i in range(1, abs(fromRow-toRow)):
-			    if fromRow > toRow and fromCol > toCol:
-				    if board[fromRow-i,fromCol-i].ChessPiece != None:
-					    return False
-			    elif fromRow > toRow and fromCol < toCol:
+	        if fromRow > toRow and fromCol > toCol:
+                    if board[fromRow-i,fromCol-i].ChessPiece != None:
+		        return False
+	            elif fromRow > toRow and fromCol < toCol:
 			        if (board[fromRow-i,fromCol+i].ChessPiece != None:
 				        return False
 			    elif fromRow<toRow and fromCol<toCol:
