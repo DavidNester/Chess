060150fa22416b2023a01468d0aea68bce340a04
DavidNester <14nesterd@gmail.com>
2014-02-03 13:21:38 -0500

    Update King.py

--- King.py
+++ King.py
@@ -1,10 +1,14 @@
 class King(ChessPiece):
+    
+    
     def __init__(self,color):
         super(King,self).__init__(self,King,color)
     
     @overrides(IsValidMove)
-    def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
-        if not (super(King,self).IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber)):
+    def override IsValidMove(board,fromRow,fromCol,
+                             toRow,toCol,turn,turnNumber):
+        if not (super(King,self).IsValidMove(board,fromRow,fromCol,
+                                             toRow,toCol,turn,turnNumber)):
             return False
         if abs(toRow-fromRow) > 1 or abs(toCol-fromRow) > 1:
             return False
