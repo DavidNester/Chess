0351926fdb72982daffcc2511439ceda3d918b5b
DavidNester <14nesterd@gmail.com>
2014-02-03 13:19:08 -0500

    Update BoardSquare.py

--- BoardSquare.py
+++ BoardSquare.py
@@ -1,7 +1,11 @@
-
 #enum ChessColors (white, black)
+White = 'W'
+Black = 'B'
+Colors = ['White','Black']
 
-class BoardSquare(object)
+class BoardSquare(object):
+  
+  
   def __init__(self, row, col)
     self.row = row
     self.col = col
