e369367561266aff8a0f34e95692af3d3dc0d425
Brandon Rhodes <brandon@rhodesmill.org>
2014-02-04 11:58:30 -0500

    Remove redundant tests

--- test_pieces.py
+++ test_pieces.py
@@ -14,37 +14,6 @@ def make_3x3_board():
 
 class PieceTests(unittest.TestCase):
 
-    def test_king_staying_in_one_place(self):
-        king = King(Black)
-        board = make_3x3_board()
-        is_valid = king.IsValidMove(board, 0,0, 0,0, Black, 6)
-        self.assertEqual(is_valid, False)
-
-    def test_king_moving_one_square(self):
-        king = King(Black)
-        board = make_3x3_board()
-        is_valid = king.IsValidMove(board, 0,0, 1,0, Black, 6)
-        self.assertEqual(is_valid, True)
-
-    def test_king_moving_back_one_square(self):
-        king = King(Black)
-        board = make_3x3_board()
-        is_valid = king.IsValidMove(board, 1,0, 0,0, Black, 6)
-        self.assertEqual(is_valid, True)
-
-    def test_king_moving_two_squares(self):
-        king = King(Black)
-        board = make_3x3_board()
-        is_valid = king.IsValidMove(board, 0,0, 2,0, Black, 6)
-        self.assertEqual(is_valid, False)
-
-    def test_king_taking_another_black_piece(self):
-        king = King(Black)
-        board = make_3x3_board()
-        board[2,0] = Square(King(Black))
-        is_valid = king.IsValidMove(board, 1,0, 2,0, Black, 6)
-        self.assertEqual(is_valid, False)
-
     def test_king_taking_a_white_piece(self):
         king = King(Black)
         board = make_3x3_board()
