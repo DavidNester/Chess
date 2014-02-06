2bef744ac6680516c51fe1288566956d7eda369d
DavidNester <14nesterd@gmail.com>
2014-02-06 00:52:35 -0500

    #3 , #5
    I believe the pawn works correctly now and the old test for the king is
    updated and commented out

--- pawn.py
+++ pawn.py
@@ -1,40 +1,45 @@
+from ChessPieces import ChessPiece, PAWN
+from BoardSquare import Colors, White, Black
+
 class Pawn(ChessPiece):
     
     
     def __init__(self,color):
-        super(Pawn,self).__init__(self,Pawn,color)
+        super(Pawn, self).__init__(PAWN, color)
     
-    @overrides(IsValidMove)
-    def override IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber):
-        if not (super(Pawn,self).IsValidMove(board,fromRow,fromCol,toRow,toCol,turn,turnNumber)):
+    def IsValidMove(self,board,fromRow,fromCol,
+                    toRow,toCol,turn,turnNumber):
+        if not (super(Pawn,self).IsValidMove(board,fromRow,fromCol,toRow,
+                                             toCol,turn,turnNumber)):
             return False
-        if turn == ChessColors.Black:
-            if fromRow > toRow:
-                return False
-        if turn == ChessColors.White:
+        if turn ==  Black:
             if fromRow < toRow:
                 return False
-        if board[toRow,toCol].ChessPiece == None:
+        if turn == White:
+            if fromRow > toRow:
+                return False
+        if board[toRow,toCol].ChessPiece is None:
             if abs(fromRow - toRow) == 1 and fromCol == toCol:
                 return True
             #en passant(complex move). might just delete it because I'm not sure it worked in original
-            elif abs(fromRow - toRow) == 1 and abs(toCol - fromCol) == 1:
-                if board[fromRow, toCol].ChessPiece.Name == ChessPieces.Pawn 
-                        and board[fromRow, toCol].ChessPiece.PieceColor != turn 
-                        and board[fromRow, toCol].ChessPiece.LastMove == turnNumber - 1:
-                    return True
-                else:
-                    return False
+            #elif abs(fromRow - toRow) == 1 and abs(toCol - fromCol) == 1:
+                    #   if board[fromRow, toCol].ChessPiece.Name == #ChessPieces.Pawn
+                    #and board[fromRow, toCol].ChessPiece.PieceColor != turn
+                    #   and board[fromRow, toCol].ChessPiece.LastMove == turnNumber - 1:
+                    #return True
+                    #else:
+            #return False
             elif abs(fromRow - toRow) == 2 and toCol == fromCol:
-                if board[fromRow,fromCol].ChessPiece.LastMove == 0:
+                if turn == Black and fromRow == 6:
+                    return True
+                elif turn == White and fromRow == 1:
                     return True
                 else:
                     return False
             else:
                 return False
-        elif board[toRow, toCol].ChessPiece != None:
-            if board[toRow, toCol].ChessPiece.PieceColor != turn 
-                    and abs(fromRow - toRow) == 1 and Math.Abs(toCol-fromCol) == 1:
+        if board[toRow, toCol].ChessPiece is not None:
+            if board[toRow, toCol].ChessPiece.color != self.color and abs(fromRow - toRow) == abs(fromCol - toCol) == 1:
                 return True
             else:
                 return False
--- test_pieces.py
+++ test_pieces.py
@@ -1,6 +1,7 @@
 import unittest
 from BoardSquare import Black, White
 from King import King
+from pawn import Pawn
 
 class Square(object):
     """Test board square."""
@@ -14,40 +15,58 @@ def make_3x3_board():
 
 class PieceTests(unittest.TestCase):
 
-    def test_king_taking_a_white_piece(self):
-        king = King(Black)
-        board = make_3x3_board()
-        board[2,0] = Square(King(White))
-        is_valid = king.IsValidMove(board, 1,0, 2,0, Black, 6)
-        self.assertEqual(is_valid, True)
+    #def test_king_taking_a_white_piece(self):
+    #    king = King(Black)
+    #    board = make_3x3_board()
+    #   board[2,0] = Square(King(White))
+    #   is_valid = king.IsValidMove(board, 1,0, 2,0, Black, 6)
+    #   self.assertEqual(is_valid, True)
+    
+    #def test_king_that_cannot_move(self):
+    #   b = Board("rnbqkbnr", #8
+    #             "pppppppp", #7
+    #             "........", #6
+    #             "........", #5
+    #             "........", #4
+    #             "........", #3
+    #             "PPPPPPPP", #2
+    #             "RNBQKBNR", #1
+    #             #abcdefgh
+    #             )
+    #   moves = b.valid_moves('e1')
+    #   self.assertEqual(moves, [])
+    #def test_king_taking_a_piece(self):
+    #     b = Board("rnbqkbnr", #8
+    #             "pppppppp", #7
+    #             "....K...", #6
+    #             "........", #5
+    #             "........", #4
+    #             "........", #3
+    #             "PPPPPPPP", #2
+    #             "RNBQ.BNR", #1
+                  #abcdefgh
+    #              )
+    #   moves = b.valid_moves('e6')
+    #   self.assertEqual(moves, ['d5', 'd6', 'd7', 'e5',
+    #                            'e7', 'f5', 'f6', 'f7'])
 
-    def test_king_that_cannot_move(self):
+    def test_pawn(self):
         b = Board("rnbqkbnr", #8
                   "pppppppp", #7
                   "........", #6
                   "........", #5
                   "........", #4
-                  "........", #3
-                  "PPPPPPPP", #2
+                  ".P......", #3
+                  "P.PPPPPP", #2
                   "RNBQKBNR", #1
                   #abcdefgh
                   )
-        moves = b.valid_moves('e1')
-        self.assertEqual(moves, [])
+        moves = b.valid_moves('b3')
+        self.assertEqual(moves, ['c4'])
 
-    def test_king_that_can_move_up(self):
-        b = Board("rnbqkbnr", #8
-                  ".p.ppp.p", #7
-                  "..p.....", #6
-                  "p.....p.", #5
-                  "....P...", #4
-                  "...P.P..", #3
-                  "PPP...PP", #2
-                  "RNBQKBNR", #1
-                  #abcdefgh
-                  )
-        moves = b.valid_moves('e1')
-        self.assertEqual(moves, ['d2', 'e2', 'f2'])
+
+
+
 
 column_names = 'abcdefgh'
 row_names = '12345678'
@@ -61,7 +80,7 @@ class Board(object):
         letter, number = square
         y = row_names.index(number)
         x = column_names.index(letter)
-        piece = King(White)  # TODO: detect what the piece really is
+        piece = Pawn(White)  # TODO: detect what the piece really is
         moves = []
         for column in column_names:
             x2 = column_names.index(column)
@@ -80,8 +99,7 @@ class Board(object):
         square = Square(piece)
         return square
 
-Queen = Bishop = Rook = Knight = Pawn = King
-
+Queen = Bishop = Rook = Knight = King
 def return_none(color):
     return None
 
