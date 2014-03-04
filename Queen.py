93227c83cd161e7bc914bb42b804adfba0c8b1b6
DavidNester <14nesterd@gmail.com>
2014-03-04 18:18:21 -0500

    Revert ef4246b..ff5bcda
    This rolls back to commit ef4246be4f09066e9c8c8e84fc7a339d84b98a82.

--- Queen.py
+++ Queen.py
@@ -1,11 +1,60 @@
 from ChessPieces import ChessPiece, QUEEN
 from BoardSquare import Colors, White, Black
 
+column_names = 'abcdefgh'
+row_names = '12345678'
+
+nw = ( -1, +1)
+sw = ( -1, -1)
+se = (+1,  -1)
+ne = (+1,  +1)
+n = ( 0, +1)
+s = ( 0, -1)
+w = (+1,  0)
+e = (-1,  0)
+
+def go(direction, x, y):
+    dx, dy = direction
+    while True:
+        x += dx
+        y += dy
+        if not (0 <= x < 8 and 0 <= y < 8):
+            break
+        if board[x,y].ChessPiece.color is self.color:
+           break
+        if board[x,y].ChessPiece.color is not self.color:
+           yield x, y
+           break
+        yield x, y
+
 class Queen(ChessPiece):
     
     
     def __init__(self,color):
         super(Queen,self).__init__(Queen,color)
+		
+	def valid_moves(self, board, position, turn, turn_number):
+        """Position looks like 'a5'."""
+        column, row = position
+        x = column_names.index(column)
+        y = row_names.index(row)
+        directions = n, s, e, w, ne, nw, se, sw
+        for direction in directions:
+            for x2, y2 in go(direction, x, y):
+                yield (x2, y2)
+				
+	def new_is_valid_move(self,board,from_row,from_col,
+						  to_row,to_col,turn,turn_number):
+        moves = []
+        #could probably change so the method receives position
+        position = "%s%s" % (row_names[from_row], column_names[from_col])
+        while True:
+            moves.append(valid_moves(board, position, turn, turn_number))
+			to_position = (to_row, to_col)
+		for item in moves:
+			if item == to_position:
+                return True
+        return False
     
     def is_valid_move(self,board,from_row,from_col,
                              to_row,to_col,turn,turn_number):
