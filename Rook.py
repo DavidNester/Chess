462145e41f498076f9025b83b15b71c09c1dd5cf
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-06 10:15:35 -0500

    Get rid of the Brandon Rook test code

--- Rook.py
+++ Rook.py
@@ -12,28 +12,3 @@ class Rook(ChessPiece):
 
     def __init__(self,color):
         super(Rook,self).__init__(Rook,color)
-
-
-if __name__ == '__main__':
-
-    def g():
-        for i in range(8):
-            yield i * i
-
-    for item in g():
-        print item
-
-    from test_pieces import Board
-
-    b = Board("rnbqkbnr", #8
-              "p.pppppp", #7
-              "........", #6
-              ".P......", #5
-              "R.......", #4
-              "........", #3
-              ".PPPPPPP", #2
-              ".NBQKBNR", #1
-              #abcdefgh
-              )
-    r = Rook(White)
-    print list(r.valid_moves(b, 'a4', White, 3))
