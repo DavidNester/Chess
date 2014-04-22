cccbfdf9e2c37920c1870fde100581dee2ee418b
DavidNester <14nesterd@gmail.com>
2014-04-22 12:08:50 -0400

    changing name of file to try to fix

---
+++ game_2048.py
@@ -0,0 +1,77 @@
+
+import pygame, sys
+from pygame import constants as c
+from twenty48 import Board_2048
+
+# Shell script for converting images:
+# for filename in *.ico; do name=$(basename $filename .ico);
+# convert $name.ico -type truecolormatte PNG32:$name.png;
+# mv $name-0.png $name.png; rm *-[12345].png; done
+
+def main():
+    new_board = Board_2048()
+    print new_board.rows
+    screen = pygame.display.set_mode((400, 400))
+    clock = pygame.time.Clock()
+    tile_images = {}
+   
+    for number in ('0', '2', '4', '8', '16', '32'):
+        filename = 'static/{}.jpg'.format(number)
+        nimage = pygame.image.load(filename)
+        nimage = nimage.convert_alpha()
+        nimage = pygame.transform.smoothscale(nimage, (76, 76))
+        tile_images[number] = nimage
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == c.KEYDOWN:
+                if event.key == c.K_ESCAPE:
+                    sys.exit(0)
+
+                # NEW:
+                elif event.key == c.K_RIGHT:
+                    new_board.move_right()
+                elif event.key == c.K_LEFT:
+                    new_board.move_left() 
+                elif event.key == c.K_UP:
+                    new_board.move_up()
+                elif event.key == c.K_DOWN:
+                    new_board.move_down()
+
+        screen.fill((255, 255, 255))
+        ###############3x3 2048        
+        
+        
+        for a in range(1,4):
+            for b in range(1,4):
+                e = (80*a)-80
+                f = (80*b)-80
+                pygame.draw.rect(screen, (0,0,0), (e,f,80,80), 1)
+        ###############3x3 2048
+        row =''
+        for i in range(0,3):
+            row += '%s' % new_board.rows[0][i]
+        for i, letter in enumerate(row):
+            x = 80 * i + 2
+            y = 2
+            screen.blit(tile_images[letter], (x, y))
+        row =''
+        for i in range(0,3):
+            row += '%s' % new_board.rows[1][i]
+        for i, letter in enumerate(row):
+            x = 80  * i + 2
+            y = 2 +80
+            screen.blit(tile_images[letter], (x, y))
+        row =''
+        for i in range(0,3):
+            row += '%s' % new_board.rows[2][i]
+        for i, letter in enumerate(row):
+            x = 80 * i + 2
+            y = 2 + 160
+            screen.blit(tile_images[letter], (x, y))
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
+
