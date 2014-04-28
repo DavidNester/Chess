b31a94b92a23805f70a87f2a6ed98e130f3332d6
DavidNester <14nesterd@gmail.com>
2014-04-28 12:10:57 -0400

    changed move_left
    The move_left function now returns moves and returns a new board instead
    of modifying the old one. I also changed the add_2() so that it should
    add a 4 10% of the time(assuming I got the syntax correct)

--- twenty48.py
+++ twenty48.py
@@ -14,27 +14,31 @@ class Board_2048(object):
             self.rows = list(rows)
     
     def move_left(self):
+		moves = list()
+		new_board = self._copy()
         for i in range(0,3):
             for j in range(0,3):
                 index = j - 1
                 zeros = True
                 while zeros:
                     if index > -1 and index < 3:
-                        if self.rows[i][index] == 0:
-                            self.rows[i][index] = self.rows[i][j]
-                            self.rows[i][j] = 0
+                        if new_board.rows[i][index] == 0:
+                            new_board.rows[i][index] = self.rows[i][j]
+                            new_board.rows[i][j] = 0
                             index -= 1
                             j -= 1
-                        elif self.rows[i][index] == self.rows[i][j]:
-                            self.rows[i][index] = self.rows[i][index]*2
-                            self.rows[i][j] = 0
+                        elif new_board.rows[i][index] == self.rows[i][j]:
+                            new_board.rows[i][index] = self.rows[i][index]*2
+                            new_board.rows[i][j] = 0
                             zeros = False
                         else:
                             zeros = False
                     else:
                         zeros = False
-        self.add_2()
-        print self.rows
+				moves.append((i,j,i,index))
+        new_board.add_2()
+		return new_board, moves
+        print new_board.rows
     
     def move_right(self):#needs to be changed
         for i in backwards:
@@ -43,21 +47,21 @@ class Board_2048(object):
                 zeros = True
                 while zeros:
                     if index > -1 and index < 3:
-                        if self.rows[i][index] == 0:
-                            self.rows[i][index] = self.rows[i][j]
-                            self.rows[i][j] = 0
+                        if new_board.rows[i][index] == 0:
+                            new_board.rows[i][index] = self.rows[i][j]
+                            new_board.rows[i][j] = 0
                             index += 1
                             j += 1
-                        elif self.rows[i][index] == self.rows[i][j]:
-                            self.rows[i][index] = self.rows[i][index]*2
-                            self.rows[i][j] = 0
+                        elif new_board.rows[i][index] == self.rows[i][j]:
+                            new_board.rows[i][index] = self.rows[i][index]*2
+                            new_board.rows[i][j] = 0
                             zeros = False
                         else:
                             zeros = False
                     else:
                         zeros = False
-        self.add_2()
-        print self.rows
+        new_board.add_2()
+        print new_board.rows
     
     def move_up(self):
         for j in range(0,3):
@@ -66,21 +70,21 @@ class Board_2048(object):
                 zeros = True
                 while zeros:
                     if index > -1 and index < 3:
-                        if self.rows[index][j] == 0:
-                            self.rows[index][j] = self.rows[i][j]
-                            self.rows[i][j] = 0
+                        if new_board.rows[index][j] == 0:
+                            new_board.rows[index][j] = self.rows[i][j]
+                            new_board.rows[i][j] = 0
                             index -= 1
                             i -= 1
-                        elif self.rows[index][j] == self.rows[i][j]:
-                            self.rows[index][j] = self.rows[index][j]*2
-                            self.rows[i][j] = 0
+                        elif new_board.rows[index][j] == self.rows[i][j]:
+                            new_board.rows[index][j] = self.rows[index][j]*2
+                            new_board.rows[i][j] = 0
                             zeros = False
                         else:
                             zeros = False
                     else:
                         zeros = False
-        self.add_2()
-        print self.rows
+        new_board.add_2()
+        print new_board.rows
     
     def move_down(self):#needs to be changed
         for j in backwards:
@@ -89,21 +93,24 @@ class Board_2048(object):
                 zeros = True
                 while zeros:
                     if index > -1 and index < 3:
-                        if self.rows[index][j] == 0:
-                            self.rows[index][j] = self.rows[i][j]
-                            self.rows[i][j] = 0
+                        if new_board.rows[index][j] == 0:
+                            new_board.rows[index][j] = self.rows[i][j]
+                            new_board.rows[i][j] = 0
                             index += 1
                             i += 1
-                        elif self.rows[index][j] == self.rows[i][j]:
-                            self.rows[index][j] = self.rows[index][j]*2
-                            self.rows[i][j] = 0
+                        elif new_board.rows[index][j] == self.rows[i][j]:
+                            new_board.rows[index][j] = self.rows[index][j]*2
+                            new_board.rows[i][j] = 0
                             zeros = False
                         else:
                             zeros = False
                     else:
                         zeros = False
-        self.add_2()
-        print self.rows
+        new_board.add_2()
+        print new_board.rows
+		
+	def _copy(self):
+		return Board(self.rows)
         
     def print_board(self):
         for i in range(0,3):
@@ -115,7 +122,12 @@ class Board_2048(object):
         open_spaces = self.open_spaces()
         number = randint(0,len(open_spaces)-1)
         i,j = open_spaces[number]
-        self.rows[i][j] = 2
+		new_value = 0
+		if rand() - .9 > 0:
+			new_value = 4
+		else:
+			new_value = 2
+        self.rows[i][j] = new_value
     
     def open_spaces(self):
         open_space = list()
